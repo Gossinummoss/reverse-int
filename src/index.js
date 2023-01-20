160863f180a5f6aa44c97d1b3247eed2b4e22826
Gossinummoss <gossinummoss@gmail.com>
2023-01-20 16:59:54 +0500

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    let  reverseNum = Number(n.toString().split("").reverse().join(""));
+    let reverseNegativeNum = Number(n.toString().split("").reverse().join("").split("-").join(''));
+
+    if (n < 0) {
+        return reverseNegativeNum
+    } else if (n >= 0) {
+        return reverseNum
+    }
 }
